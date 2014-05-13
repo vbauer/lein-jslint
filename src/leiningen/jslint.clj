6036102547eb4580ec5bb8376e8fb054f199d58b
Vladislav Bauer <bauer.vlad@gmail.com>
2014-05-14 02:13:49 +0700

    add :excludes option, fix bikeshed issues

--- src/leiningen/jslint.clj
+++ src/leiningen/jslint.clj
@@ -46,6 +46,7 @@
 (defn- config [project] (opt project :config {}))
 (defn- config-file [project] (opt project :config-file DEF_CONFIG_FILE))
 (defn- include-files [project] (find-files (vec (opt project :includes nil))))
+(defn- exclude-files [project] (find-files (vec (opt project :excludes nil))))
 
 
 ; Internal API: Runner
@@ -54,24 +55,37 @@
   (json/generate-string
    (merge (config project) DEF_CONFIG)))
 
+(defn- sources-list [project args]
+  (let [includes (include-files project)
+        sources (remove empty? (concat (apply vec args) includes))
+        excludes (apply hash-set (exclude-files project))
+        target (remove (fn [x] (or (empty? x)
+                                   (contains? excludes x))) sources)]
+    (if (empty? target)
+      (do
+        (println "No JS files specified.")
+        (main/abort))
+      target)))
+
 (defn- invoke [project & args]
   (process/exec
    (project :root)
    (cons "jslint" args)))
 
-(defn- proc [project]
+(defn- proc [project & args]
   (try
     (npm/environmental-consistency project)
     (let [file (config-file project)
           content (generate-config-file project)
-          includes (include-files project)]
+          sources (sources-list project args)]
       (npm/with-json-file file content project
-                          (apply invoke project includes)))
+                          (apply invoke project sources)))
     (catch Throwable t
-      (println (joine (str "Can't execute jslint application: " (.getMessage t))
-                      "Check that JSLint is:"
-                      " - installed correctly: npm install jslint -g"
-                      " - configured correctly: https://github.com/vbauer/lein-jslint.git"))
+      (println
+       (joine (str "Can't execute jslint application: " (.getMessage t))
+              "Something is wrong:"
+              " - installation: npm install jslint -g"
+              " - configuration: https://github.com/vbauer/lein-jslint"))
       (main/abort))))
 
 
@@ -83,7 +97,7 @@
   [project & args]
   (if (= args ["help"])
     (println (help/help-for "jslint"))
-    (proc project)))
+    (proc project args)))
 
 
 ; External API: Hooks
