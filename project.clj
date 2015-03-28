bba65b848a2f05d35014414399993dca21f60d05
Vladislav Bauer <bauer.vlad@gmail.com>
2015-03-28 14:33:11 +0600

    lein-release plugin: preparing 0.1.8 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-jslint "0.1.8-SNAPSHOT"
+(defproject lein-jslint "0.1.8"
   :description "A Leiningen plugin for running JS code through JSLint."
   :url "https://github.com/vbauer/lein-jslint"
   :license {:name "Eclipse Public License"
